c1145e892aed6aa005d568ca945a717c7f0f9380
Ben Lovell <benjamin.lovell@gmail.com>
2016-06-27 13:50:07 +0100

    Simple validation for slots
    Provides simple validation for slots. This is nothing more than a basic
    sanity check for now.

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,5 +1,14 @@
 class Slot < ActiveRecord::Base
+  PERMITTED_TIME_REGEX = /\A\d{4}\z/
+  PERMITTED_PRIORITIES = [1, 2, 3].freeze
+
   belongs_to :booking_request
+
+  validates :date, presence: true
+  validates :priority, inclusion: { in: PERMITTED_PRIORITIES }
+  validates :from, format: { with: PERMITTED_TIME_REGEX }
+  validates :to, format: { with: PERMITTED_TIME_REGEX }
+  validate :validate_from_before_to
 
   def morning?
     from < '1300'
@@ -11,5 +20,11 @@ class Slot < ActiveRecord::Base
 
   def period
     morning? ? 'morning' : 'afternoon'
+  end
+
+  private
+
+  def validate_from_before_to
+    errors.add(:base, 'from must be before to') unless from < to
   end
 end
