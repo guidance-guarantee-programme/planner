4b67290660f91eeea50560902054eb239cfc6afc
Ben Barnett <ben@brushfiredesign.net>
2016-06-30 15:42:17 +0100

    UI for booking request fulfilment page
    Needs wiring up to the backend and location information
    from the Locations API.

--- app/controllers/booking_requests_controller.rb
+++ app/controllers/booking_requests_controller.rb
@@ -11,4 +11,8 @@ class BookingRequestsController < ApplicationController
   def unfulfilled_booking_requests
     current_user.unfulfilled_booking_requests.page(params[:page])
   end
+
+  def show
+    @booking_request = BookingRequest.find(params[:id])
+  end
 end
