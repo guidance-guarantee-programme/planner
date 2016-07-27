8c7f204af846e888ee368ee0dcfd70d2c041122b
Ben Barnett <ben@brushfiredesign.net>
2016-07-27 10:18:33 +0100

    Configure GovUK admin template to show signout and flash messages
    Adds an initializer for govuk_admin_template and brings in `plek`.
    This change means an environment variable of `OAUTH_ROOT_URL`
    is required, which is where the user will be redirected to when
    they sign out. This falls back to http://localhost:3001.

--- config/initializers/gds_sso.rb
+++ config/initializers/gds_sso.rb
@@ -2,7 +2,7 @@ GDS::SSO.config do |config|
   config.user_model = 'User'
 
   config.oauth_id = ENV['OAUTH_ID']
-  config.oauth_root_url = ENV['OAUTH_ROOT_URL']
+  config.oauth_root_url = ENV.fetch('OAUTH_ROOT_URL', 'http://localhost:3001')
   config.oauth_secret = ENV['OAUTH_SECRET']
 
   config.cache = Rails.cache
