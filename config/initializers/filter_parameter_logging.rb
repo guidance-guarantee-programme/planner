be549ab387bff4e003a1b595db0efd3605c633f7
Ben Lovell <benjamin.lovell@gmail.com>
2016-08-01 20:27:42 +0100

    Strip personal information from logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters = %w(
+  password
+  name
+  email
+  phone
+  memorable_word
+)
