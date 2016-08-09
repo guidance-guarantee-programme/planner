1ce96e417a0025f57b697debd74b3e58dd0bf6f5
Ben Barnett <ben@brushfiredesign.net>
2016-08-09 16:58:52 +0100

    Tag emails with environment they were sent from
    As emails are delivered using the production mailgun environment,
    this allows us to identify the source and whether to include them
    in our dashboards.

--- spec/support/shared_examples_for_mailers.rb
+++ spec/support/shared_examples_for_mailers.rb
@@ -1,5 +1,6 @@
 RSpec.shared_examples 'mailgun identified email' do
   it 'renders the mailgun identification header' do
     expect(mail['X-Mailgun-Variables'].value).to include('"online_booking":true')
+    expect(mail['X-Mailgun-Variables'].value).to include('"environment":"test"')
   end
 end
