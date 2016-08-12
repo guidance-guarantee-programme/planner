c59f6ee02561d152b2737015a6cd8cfcf9a5e5f9
Ben Lovell <benjamin.lovell@gmail.com>
2016-08-12 15:30:02 +0100

    Include the sender's name in mailings

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,7 +3,7 @@ require 'mailgun_headers'
 class ApplicationMailer < ActionMailer::Base
   include MailgunHeaders
 
-  default from: 'appointments@pensionwise.gov.uk'
+  default from: 'Pension Wise Appointments <appointments@pensionwise.gov.uk>'
 
   layout 'mailer'
 end
