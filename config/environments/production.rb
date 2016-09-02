d137d9802b889dfa6a0a192847d829915de41655
Ben Lovell <benjamin.lovell@gmail.com>
2016-09-02 21:31:51 +0100

    Remove `serve_static_files` deprecations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # Configure static file server for tests with Cache-Control for performance.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
