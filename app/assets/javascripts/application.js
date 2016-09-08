37bfd2df7bfab99929f773407b63a231fd241914
Ben Barnett <ben@brushfiredesign.net>
2016-09-08 14:59:15 +0100

    Add dummy activity feed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,16 @@
 //= require fullcalendar-scheduler/dist/scheduler
 
 //= require_tree .
+
+
+(function($) {
+  var $collapseItems = $('main [data-toggle="collapse"]');
+  $collapseItems.removeClass('hidden');
+
+  $collapseItems.each(function() {
+    $item = $(this);
+    var targetId = $item.attr('data-target') || $item.attr('href');
+
+    $(targetId).addClass('collapse');
+  });
+})(jQuery);
