022d62727f6f7a95dec31950db426bb4df612756
Ben Lovell <benjamin.lovell@gmail.com>
2017-03-20 10:56:50 +0000

    Clear only cached locations
    Due to recent changes to `booking_locations` we now prefix booking
    locations when read-through cached by the gem, so in turn we can be more
    specific about which cache entries we clear.

--- app/lib/batch_booking_request_reassignment.rb
+++ app/lib/batch_booking_request_reassignment.rb
@@ -8,16 +8,12 @@ class BatchBookingRequestReassignment
     affected_bookings
       .update_all(booking_location_id: booking_location_id) # rubocop:disable Rails/SkipsModelValidations
 
-    expire_cached_locations
+    BookingLocations.clear_cache
   end
 
   private
 
   attr_reader :booking_location_id, :location_id
-
-  def expire_cached_locations
-    Rails.cache.clear
-  end
 
   def affected_bookings
     @affected_bookings ||= BookingRequest.where(location_id: location_id)
