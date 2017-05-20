b5aab4e535306d4c430843fc3a4583d01c6725d2
Ben Lovell <benjamin.lovell@gmail.com>
2017-05-20 13:22:07 +0100

    Configure bootsnap for snappy boots
    Yields at least a 50% reduction in boot times in both development and
    test. See here for details: https://github.com/Shopify/bootsnap

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,20 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+if %w(development test).include?(ENV.fetch('RAILS_ENV'))
+  require 'bootsnap'
+
+  # These features are Mac only for the timebeing
+  DARWIN = `uname`.chomp == 'Darwin'
+
+  Bootsnap.setup(
+    cache_dir: 'tmp/cache',
+    development_mode: true,
+    load_path_cache: true,
+    autoload_paths_cache: true,
+    disable_trace: false,
+    compile_cache_iseq: DARWIN,
+    compile_cache_yaml: DARWIN
+  )
+end
