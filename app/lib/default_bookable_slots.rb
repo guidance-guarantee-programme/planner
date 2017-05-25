25e8dc1eb7dc932b1424b7fa271e7d96009c456f
Ben Lovell <benjamin.lovell@gmail.com>
2017-05-25 11:03:04 +0100

    Expose a public bookable slots API
    Exposes the slots API at:
    `/api/v1/locations/{location}/bookable_slots.json`
    
    For now, the `{location}` parameter is ignored but will eventually be
    used to determine whether the location has an associated schedule.
    
    Example payload:
    
    ```
    [
      {
        "date": "2017-05-30",
        "start": "0900",
        "end": "1300"
      },
      {
        "date": "2017-05-30",
        "start": "1300",
        "end": "1700"
      }
    ]
    
    // and so on
    ```

---
+++ app/lib/default_bookable_slots.rb
@@ -0,0 +1,38 @@
+class DefaultBookableSlots
+  Instance = Struct.new(:date, :start, :end)
+
+  attr_reader :from
+  attr_reader :to
+
+  def initialize(from = Date.current, to = from.advance(weeks: 6))
+    @from = from
+    @to = to
+  end
+
+  def call
+    available_days.map do |date|
+      [
+        Instance.new(date.iso8601, '0900', '1300'),
+        Instance.new(date.iso8601, '1300', '1700')
+      ]
+    end.flatten
+  end
+
+  private
+
+  def available_days
+    (grace_period_end..to).reject { |day| day.on_weekend? || bank_holiday?(day) }
+  end
+
+  def bank_holiday?(date)
+    BANK_HOLIDAYS.include?(date)
+  end
+
+  def grace_period_end
+    if from.monday? || from.tuesday?
+      from.advance(days: 3)
+    else
+      from.next_week(GRACE_PERIODS[from.wday])
+    end
+  end
+end
