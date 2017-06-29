e566b95da8dc8de1f9dc5bc039d585054295edd2
Ben Lovell <benjamin.lovell@gmail.com>
2017-06-29 16:07:16 +0100

    Include number of chosen slots for slack ping
    Pings slack with the location and number of slots the customer chose.

--- app/jobs/slack_pinger_job.rb
+++ app/jobs/slack_pinger_job.rb
@@ -8,7 +8,7 @@ class SlackPingerJob < ActiveJob::Base
     actual_location  = booking_location.name_for(booking_request.location_id)
 
     hook = WebHook.new(hook_uri)
-    hook.call(payload(actual_location))
+    hook.call(payload(actual_location, booking_request.slots.size))
   end
 
   private
@@ -17,11 +17,11 @@ class SlackPingerJob < ActiveJob::Base
     ENV['BOOKING_REQUESTS_SLACK_PINGER_URI']
   end
 
-  def payload(actual_location)
+  def payload(actual_location, slot_count)
     {
       username: 'frank',
       channel: '#online-bookings',
-      text: ":rotating_light: #{actual_location} :rotating_light:",
+      text: ":rotating_light: #{actual_location} (#{slot_count} slots) :rotating_light:",
       icon_emoji: ':frank:'
     }
   end
