135b07c6a6e757bec30ae7b308a4530eb3456f82
Ben Lovell <benjamin.lovell@gmail.com>
2017-07-27 14:40:43 +0100

    Use system phantomjs where present
    This is preferable to `phantomjs-binaries` now the `phantomjs` gem uses
    a CDN without an over-zealous rate limit.

--- spec/support/poltergeist.rb
+++ spec/support/poltergeist.rb
@@ -1,4 +1,11 @@
 require 'capybara/poltergeist'
 
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(
+    app,
+    phantomjs: Phantomjs.path
+  )
+end
+
 Capybara.javascript_driver = :poltergeist
 Capybara.default_max_wait_time = 20
