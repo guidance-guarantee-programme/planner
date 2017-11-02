14b24cc5efbb4bc451efaa402b126bdfe3f12c1e
Ben Lovell <benjamin.lovell@gmail.com>
2017-11-02 13:42:00 +0000

    Remove CITA slots over the Christmas period
    These have likely been created by schedules and regeneration of
    existing schedules that intersect with this period. Since the CITA
    locations have blanket closures over this period we should remove all
    available slots.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019145702) do
+ActiveRecord::Schema.define(version: 20171102132151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
