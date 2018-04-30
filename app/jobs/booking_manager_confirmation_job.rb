64742ee6da1a599cb4d3a5e712d2ffdda5384130
Andrew White <andrew.white@unboxed.co>
2018-04-30 11:28:42 +0100

    Remove redundant location api lookup

--- app/jobs/booking_manager_confirmation_job.rb
+++ app/jobs/booking_manager_confirmation_job.rb
@@ -4,8 +4,7 @@ class BookingManagerConfirmationJob < ActiveJob::Base
   queue_as :default
 
   def perform(booking_request)
-    booking_location = BookingLocations.find(booking_request.location_id)
-    booking_managers = User.active.where(organisation_content_id: booking_location.id)
+    booking_managers = User.active.where(organisation_content_id: booking_request.location_id)
 
     raise BookingManagersNotFoundError unless booking_managers.present?
 
--- spec/jobs/booking_manager_confirmation_job_spec.rb
+++ spec/jobs/booking_manager_confirmation_job_spec.rb
@@ -1,12 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe BookingManagerConfirmationJob, '#perform' do
-  let(:booking_request)  { double(location_id: 'whelp') }
-  let(:booking_location) { double(id: 'badbeef') }
-
-  before do
-    allow(BookingLocations).to receive(:find).and_return(booking_location)
-  end
+  let(:location_id) { 'ac7112c3-e3cf-45cd-a8ff-9ba827b8e7ef' }
+  let(:booking_request) { create(:booking_request, location_id: location_id) }
 
   subject { described_class.new.perform(booking_request) }
 
@@ -19,8 +15,6 @@ RSpec.describe BookingManagerConfirmationJob, '#perform' do
   context 'when booking manager(s) are found' do
     before { create(:hackney_booking_manager) }
 
-    let(:booking_location) { double(id: 'ac7112c3-e3cf-45cd-a8ff-9ba827b8e7ef') }
-
     it 'fans out a notification job for each booking manager' do
       assert_enqueued_jobs(1) { subject }
     end
@@ -28,8 +22,6 @@ RSpec.describe BookingManagerConfirmationJob, '#perform' do
 
   context 'with only inactive booking managers' do
     before { create(:hackney_booking_manager, disabled: true) }
-
-    let(:booking_location) { double(id: 'ac7112c3-e3cf-45cd-a8ff-9ba827b8e7ef') }
 
     it 'raises an error' do
       expect { subject }.to raise_error(BookingManagersNotFoundError)
