422852df75c251fd7def1cbd50fda6ae0b52f90a
Ben Lovell <benjamin.lovell@gmail.com>
2018-06-12 13:31:03 +0100

    Rename from 'Appointment Planner'
    We've never referred to this system as 'Appointment Planner' in comms
    or otherwise, free up valuable real-estate in the navigation
    bar and make the naming consistent with code artifacts.

--- config/initializers/govuk_admin_template.rb
+++ config/initializers/govuk_admin_template.rb
@@ -1,7 +1,7 @@
 GovukAdminTemplate.environment_style = Rails.env.staging? ? 'preview' : ENV['RAILS_ENV']
 
 GovukAdminTemplate.configure do |c|
-  c.app_title = 'Appointment Planner'
+  c.app_title = 'Planner'
   c.show_flash = true
   c.show_signout = true
 end
