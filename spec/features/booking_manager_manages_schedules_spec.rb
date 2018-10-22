41abf3ce4d85e35da9e425304a9c3ce5492c1479
Ben Lovell <benjamin.lovell@gmail.com>
2018-10-22 13:42:21 +0100

    Do not allow schedules for pilot locations

--- spec/features/booking_manager_manages_schedules_spec.rb
+++ spec/features/booking_manager_manages_schedules_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature 'Booking manager manages schedules' do
     given_the_user_identifies_as_hackneys_booking_manager do
       when_they_view_their_schedules
       then_they_see_default_schedules_across_their_locations
-      when_they_manage_the_booking_location_schedule
+      when_they_manage_the_location_schedule
       and_they_mark_some_days_unavailable
       when_they_save_the_schedule
       then_slot_generation_is_scheduled
@@ -25,10 +25,7 @@ RSpec.feature 'Booking manager manages schedules' do
 
     # the booking location
     expect(@page.schedules.first.location.text).to eq('Hackney')
-    expect(@page.schedules.first.manage['href']).to eq(
-      new_schedule_path(location_id: 'ac7112c3-e3cf-45cd-a8ff-9ba827b8e7ef')
-    )
-
+    expect(@page.schedules.first).to have_no_manage
     # the first child location
     expect(@page.schedules.second.location.text).to eq('Dalston')
     expect(@page.schedules.second.manage['href']).to eq(
@@ -36,13 +33,13 @@ RSpec.feature 'Booking manager manages schedules' do
     )
   end
 
-  def when_they_manage_the_booking_location_schedule
-    @page.schedules.first.manage.click
+  def when_they_manage_the_location_schedule
+    @page.schedules.second.manage.click
   end
 
   def and_they_mark_some_days_unavailable
     @page = Pages::Schedule.new
-    @page.load(location_id: 'ac7112c3-e3cf-45cd-a8ff-9ba827b8e7ef')
+    @page.load(location_id: '183080c6-642b-4b8f-96fd-891f5cd9f9c7')
     expect(@page).to be_loaded
 
     @page.monday_am.click
@@ -61,7 +58,7 @@ RSpec.feature 'Booking manager manages schedules' do
   def and_they_see_the_schedule_summarised_accurately # rubocop:disable Metrics/AbcSize
     @page = Pages::Schedules.new
 
-    @page.schedules.first.summary.tap do |summary|
+    @page.schedules.second.summary.tap do |summary|
       expect(summary.monday_am).to be_closed
       expect(summary.wednesday_pm).to be_closed
       expect(summary.friday_am).to be_closed
