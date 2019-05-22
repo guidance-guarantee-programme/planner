7e8771e006c9b7fb77e30bd3611defe2688c5c51
Ben Lovell <benjamin.lovell@gmail.com>
2019-05-22 11:43:15 +0100

    Allocate slots when they are missing
    When a booking manager alters the date/time and/or the guider of an
    already booked appointment we will now create an underlying slot where
    no existing overlapping slot is present.

--- spec/forms/edit_appointment_form_spec.rb
+++ spec/forms/edit_appointment_form_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe EditAppointmentForm do
   let(:hackney) { BookingLocations.find('ac7112c3-e3cf-45cd-a8ff-9ba827b8e7ef') }
-  let(:underlying_appointment) { build_stubbed(:appointment) }
+  let(:underlying_appointment) { create(:appointment) }
   let(:appointment) do
     LocationAwareEntity.new(
       entity: underlying_appointment,
@@ -20,5 +20,23 @@ RSpec.describe EditAppointmentForm do
 
   it 'delegates the agent to the underlying booking request' do
     expect(subject.agent).to eq(appointment.booking_request.agent)
+  end
+
+  context 'when no underlying slot exists after reallocation' do
+    it 'creates the missing realtime slot' do
+      # creates the missing slot
+      expect { subject.update(proceeded_at: '2019-05-22 13:00 UTC') }.to change { BookableSlot.count }.by(1)
+
+      # doesn't create another, overlapping slot
+      expect { subject.update(proceeded_at: '2019-05-22 12:45 UTC') }.not_to(change { BookableSlot.count })
+
+      # creates another slot for the guider change
+      expect { subject.update(guider_id: 2) }.to change { BookableSlot.count }.by(1)
+
+      # creates another slot for the location and date change
+      expect do
+        subject.update(location_id: '183080c6-642b-4b8f-96fd-891f5cd9f9c7', proceeded_at: '2019-05-22 14:15 UTC')
+      end.to change { BookableSlot.count }.by(1)
+    end
   end
 end
