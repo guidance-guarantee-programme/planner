4aa9830c5c4fb72920ad61bafdbcd0de334b05e7
Ben Lovell <benjamin.lovell@gmail.com>
2019-07-30 12:00:12 +0100

    Ping slack with pension provider if specified

--- app/jobs/slack_pinger_job.rb
+++ app/jobs/slack_pinger_job.rb
@@ -10,7 +10,7 @@ class SlackPingerJob < ActiveJob::Base
     actual_location  = booking_location.name_for(booking_request_or_appointment.location_id)
 
     hook = WebHook.new(hook_uri)
-    hook.call(payload(actual_location, booking_request_or_appointment.realtime?))
+    hook.call(payload(actual_location, booking_request_or_appointment))
   end
 
   private
@@ -19,12 +19,13 @@ class SlackPingerJob < ActiveJob::Base
     ENV['BOOKING_REQUESTS_SLACK_PINGER_URI']
   end
 
-  def payload(actual_location, realtime)
-    emoji = realtime ? ':zap:' : ':rotating_light:'
+  def payload(actual_location, booking_request_or_appointment)
+    pension_provider = PensionProvider[booking_request_or_appointment]
+
     {
       username: 'frank',
       channel: '#online-bookings',
-      text: "#{emoji} #{actual_location} #{emoji}",
+      text: ":zap: #{actual_location} #{pension_provider} :zap:",
       icon_emoji: ':frank:'
     }
   end
