12b6c84f23c914491d4dfe26604d00b047766a5e
Ben Lovell <benjamin.lovell@gmail.com>
2022-06-13 12:43:14 +0100

    Update email templates and confirmation
    This covers the customer confirmation/update email and associated
    layouts plus shared templates.

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -4,9 +4,40 @@ module EmailHelper
       :p,
       capture(&block),
       style: [
-        'color: #0B0C0C',
-        'font-family: Helvetica, Arial, sans-serif',
+        'color: #000B3B',
+        'font-family: Calibri, Arial, sans-serif',
         'margin: 15px 0',
+        'font-size: 16px',
+        'line-height: 1.315789474'
+      ].join(';')
+    )
+  end
+
+  def h2(&block) # rubocop:disable MethodLength
+    content_tag(
+      :h2,
+      capture(&block),
+      style: [
+        'color: #0F19A0 !important',
+        'font-family: Calibri, Arial, sans-serif',
+        'margin: 15px 0',
+        'font-weight: 700',
+        'font-size: 18px',
+        'line-height: 1.111111111',
+        'padding: 15px 0'
+      ].join(';')
+    )
+  end
+
+  def ul(&block) # rubocop:disable MethodLength
+    content_tag(
+      :ul,
+      capture(&block),
+      style: [
+        'color: #0B0C0C',
+        'font-family: Calibri, Arial, sans-serif',
+        'margin-top: 15px',
+        'margin-bottom: 15px',
         'font-size: 16px',
         'line-height: 1.315789474'
       ].join(';')
