4bcf5655d87efc9cdd9cc00381dee1677f0ce155
Ben Lovell <benjamin.lovell@gmail.com>
2022-07-17 18:44:27 +0100

    Add August 22 bank holiday

--- spec/lib/grace_period_spec.rb
+++ spec/lib/grace_period_spec.rb
@@ -4,22 +4,22 @@ RSpec.describe GracePeriod do
   it 'is correct for PWNI' do
     @lookup = create(:organisation_lookup, :pwni)
 
-    travel_to '2020-03-13'.to_date do
+    travel_to '2022-08-25'.to_date do
       pwni = described_class.new(@lookup.location_id)
 
-      # jumps past the 17th which is a holiday
-      expect(pwni.start).to eq('2020-03-18'.to_date)
+      # jumps past the holiday
+      expect(pwni.start).to eq('2022-08-30'.to_date)
     end
   end
 
   it 'is correct for CAS' do
     @lookup = create(:organisation_lookup, :cas)
 
-    travel_to '2020-04-08'.to_date do
+    travel_to '2022-08-25'.to_date do
       cas = described_class.new(@lookup.location_id)
 
-      # jumps past the 10th which is a holiday and the following weekend
-      expect(cas.start).to eq('2020-04-13'.to_date)
+      # lands on an English/Welsh bank holiday
+      expect(cas.start).to eq('2022-08-29'.to_date)
     end
   end
 end
