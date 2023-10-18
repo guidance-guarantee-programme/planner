e047a570ed3d2ba8cd7db938f225ecd6ef99c4e9
emilysatodossantos <emily.satodossantos@solirius.com>
2023-10-18 16:13:55 +0100

    Explicit block argument

--- app/helpers/appointment_helper.rb
+++ app/helpers/appointment_helper.rb
@@ -3,10 +3,10 @@ module AppointmentHelper
     booking.postal_confirmation? ? 'letter' : 'email'
   end
 
-  def field_with_errors_wrapper(form, field, klass)
+  def field_with_errors_wrapper(form, field, klass, &block)
     error_class = form.object.errors[field].any? ? "field_with_errors #{klass}" : klass
 
-    content_tag(:div, class: error_class) { yield }
+    content_tag(:div, class: error_class, &block)
   end
 
   def required_label(field = nil)
