124ed1ab084e0c62a4bd5f0fc9ad1b617860c605
Ben Lovell <benjamin.lovell@gmail.com>
2024-10-23 16:41:44 +0100

    Guard against the absence of an email
    This job is scheduled but ought to do nothing if the underlying
    appointment does not have an email associated.

--- app/jobs/bsl_customer_exit_poll_job.rb
+++ app/jobs/bsl_customer_exit_poll_job.rb
@@ -2,6 +2,8 @@ class BslCustomerExitPollJob < ActiveJob::Base
   queue_as :default
 
   def perform(appointment)
+    return unless appointment.email?
+
     booking_location = BookingLocations.find(appointment.location_id)
 
     Appointments.bsl_customer_exit_poll(appointment, booking_location).deliver_now
