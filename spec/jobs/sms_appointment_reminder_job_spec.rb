0b5e1b5338bb11f46ef953a81de058a755e11d77
Ben Lovell <benjamin.lovell@gmail.com>
2026-02-04 16:47:45 +0000

    Customise SMS reminder template for video

--- spec/jobs/sms_appointment_reminder_job_spec.rb
+++ spec/jobs/sms_appointment_reminder_job_spec.rb
@@ -4,20 +4,42 @@ RSpec.describe SmsAppointmentReminderJob, '#perform' do
   let(:appointment) { create(:appointment, phone: '07715 930 444') }
   let(:client) { double(Notifications::Client, send_sms: true) }
 
-  it 'sends an SMS' do
-    expect(client).to receive(:send_sms).with(
-      phone_number: '07715 930 444',
-      template_id: SmsAppointmentReminderJob::TEMPLATE_ID,
-      reference: appointment.reference,
-      personalisation: {
-        date: '2:00pm, 20 Jun 2016',
-        location: 'Hackney'
-      }
-    )
+  context 'when sending a normal appointment reminder' do
+    it 'sends an SMS with the correct template and personalisation' do
+      expect(client).to receive(:send_sms).with(
+        phone_number: '07715 930 444',
+        template_id: SmsAppointmentReminderJob::TEMPLATE_ID,
+        reference: appointment.reference,
+        personalisation: {
+          date: '2:00pm, 20 Jun 2016',
+          location: 'Hackney'
+        }
+      )
 
-    described_class.new.perform(appointment)
+      described_class.new.perform(appointment)
 
-    expect(appointment.activities.first).to be_an(SmsReminderActivity)
+      expect(appointment.activities.first).to be_an(SmsReminderActivity)
+    end
+  end
+
+  context 'when sending a video appointment reminder' do
+    let(:appointment) { create(:appointment, :video, phone: '07715 930 444') }
+
+    it 'sends an SMS with the correct template and personalisation' do
+      expect(client).to receive(:send_sms).with(
+        phone_number: '07715 930 444',
+        template_id: SmsAppointmentReminderJob::VIDEO_TEMPLATE_ID,
+        reference: appointment.reference,
+        personalisation: {
+          date: '2:00pm, 20 Jun 2016',
+          location: 'Hackney'
+        }
+      )
+
+      described_class.new.perform(appointment)
+
+      expect(appointment.activities.first).to be_an(SmsReminderActivity)
+    end
   end
 
   before do
