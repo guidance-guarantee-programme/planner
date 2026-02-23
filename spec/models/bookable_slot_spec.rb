2db2f78477b76944f1956f6b430e60fb0a41ea63
Ben Lovell <benjamin.lovell@gmail.com>
2026-02-23 11:52:04 +0000

    Allow Ops video slots to be created

--- spec/models/bookable_slot_spec.rb
+++ spec/models/bookable_slot_spec.rb
@@ -21,6 +21,12 @@ RSpec.describe BookableSlot do
     expect(build(:bookable_slot, start_at: Time.zone.parse('2026-03-01 09:00'))).to be_invalid
   end
 
+  it 'does permit video slots after 28/02/2026' do
+    @schedule = build(:schedule, :ops)
+
+    expect(build(:bookable_slot, schedule: @schedule, start_at: Time.zone.parse('2026-03-01 09:00'))).to be_valid
+  end
+
   describe 'validations' do
     it 'does not allow overlapping slots for a particular guider' do
       @persisted = create(:bookable_slot) # 09:00 - 10:00
