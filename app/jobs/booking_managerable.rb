2c1d13992a89398f2c3a7a06b6a85f0cfc5440ad
Ben Lovell <benjamin.lovell@gmail.com>
2026-02-23 11:43:22 +0000

    Allow ops mailbox alias to be configurable

--- app/jobs/booking_managerable.rb
+++ app/jobs/booking_managerable.rb
@@ -1,7 +1,10 @@
 module BookingManagerable
   def booking_managers_for(booking_location_id)
     return [Appointment::CAS_BOOKING_MANAGER_ALIAS] if booking_location_id == Appointment::CAS_BOOKING_LOCATION_ID
-    return [Appointment::OPS_BOOKING_MANAGER_ALIAS] if booking_location_id == Appointment::OPS_BOOKING_LOCATION_ID
+
+    if booking_location_id == Appointment::OPS_BOOKING_LOCATION_ID
+      return ENV.fetch('OPS_BOOKING_MANAGER_ALIASES') { Appointment::OPS_BOOKING_MANAGER_ALIAS }.split(',')
+    end
 
     User.booking_managers(booking_location_id).pluck(:email)
   end
