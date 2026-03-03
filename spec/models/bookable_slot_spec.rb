41faa5d4ed980c57b5d31fc5af87e5e502f2f81a
Ben Lovell <benjamin.lovell@gmail.com>
2026-03-03 17:19:00 +0000

    Resolve timing issue with failing spec

--- spec/models/bookable_slot_spec.rb
+++ spec/models/bookable_slot_spec.rb
@@ -29,16 +29,18 @@ RSpec.describe BookableSlot do
 
   describe 'validations' do
     it 'does not allow overlapping slots for a particular guider' do
-      @persisted = create(:bookable_slot) # 09:00 - 10:00
+      travel_to '2026-01-01 09:00' do
+        @persisted = create(:bookable_slot) # 09:00 - 10:00
 
-      # exact duplicate
-      expect(build(:bookable_slot)).to be_invalid
-      # intersects with the start
-      expect(build(:bookable_slot, start_at: time_today('08:30'))).to be_invalid
-      # intersects with the end
-      expect(build(:bookable_slot, start_at: time_today('09:30'))).to be_invalid
-      # overlaps at the last minute
-      expect(build(:bookable_slot, start_at: time_today('10:00'))).to be_valid
+        # exact duplicate
+        expect(build(:bookable_slot)).to be_invalid
+        # intersects with the start
+        expect(build(:bookable_slot, start_at: time_today('08:30'))).to be_invalid
+        # intersects with the end
+        expect(build(:bookable_slot, start_at: time_today('09:30'))).to be_invalid
+        # overlaps at the last minute
+        expect(build(:bookable_slot, start_at: time_today('10:00'))).to be_valid
+      end
     end
   end
 end
