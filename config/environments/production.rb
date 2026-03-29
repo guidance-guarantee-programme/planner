b734b659cc1618015ca63069c0d6399e40b8370e
Ben Lovell <benjamin.lovell@gmail.com>
2026-03-29 20:40:14 +0100

    Enable harmony mode for ES6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
   }
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
